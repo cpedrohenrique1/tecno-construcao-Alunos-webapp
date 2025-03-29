022e2158f2da0c3ad8f8a5dbb6b99090b5133d28
cpedrohenrique1 <phpedrohenrique474@gmail.com>
2025-03-29 17:36:58 -0300

    Exercicio3

--- index.js
+++ index.js
@@ -1,56 +1,76 @@
 let alunos = [];
 
-function buttonCadastrar() {
-    const nome = document.getElementById("nome").value;
-    const idade = parseInt(document.getElementById("idade").value);
-    const curso = document.getElementById("cursos").value;
-    const notaFinal = parseFloat(document.getElementById("notaFinal").value);
+document.addEventListener("DOMContentLoaded", ()=> {
+    const buttonCadastrar = (aluno)=> {
+        alunos.push(aluno);
+        atualizarTabela();
+    }
 
-    alunos.push(new Aluno(nome, idade, curso, notaFinal));
-    atualizarTabela();
-}
+    const atualizarTabela = () =>{
+        const tabela = document.getElementById("alunosTableBody");
+        tabela.innerHTML = "";
 
-function atualizarTabela(){
-    const tabela = document.getElementById("alunosTableBody");
-    tabela.innerHTML = "";
+        for (let i = 0; i < alunos.length; i++) {
+            const aluno = alunos[i];
+            const linha = tabela.insertRow(0);
+            const celulaNome = linha.insertCell(0);
+            const celulaIdade = linha.insertCell(1);
+            const celulaCurso = linha.insertCell(2);
+            const celulaNotaFinal = linha.insertCell(3);
+            const celulaAcao = linha.insertCell(4);
 
-    for (let i = 0; i < alunos.length; i++) {
-        const aluno = alunos[i];
-        const linha = tabela.insertRow(0);
-        const celulaNome = linha.insertCell(0);
-        const celulaIdade = linha.insertCell(1);
-        const celulaCurso = linha.insertCell(2);
-        const celulaNotaFinal = linha.insertCell(3);
-        const celulaAcao = linha.insertCell(4);
-
-        celulaNome.textContent = aluno.nome;
-        celulaIdade.textContent = aluno.idade;
-        celulaCurso.textContent = aluno.curso;
-        celulaNotaFinal.textContent = aluno.notaFinal;
-        celulaAcao.innerHTML = `
-            <button onclick="editarAluno(${i})">Editar</button>
-            <button onclick="excluirAluno(${i})">Excluir</button>
+            celulaNome.textContent = aluno.nome;
+            celulaIdade.textContent = aluno.idade;
+            celulaCurso.textContent = aluno.curso;
+            celulaNotaFinal.textContent = aluno.notaFinal;
+            celulaAcao.innerHTML = `
+            <button class="editarButton" data-index="${i}">Editar</button>
+            <button class="excluirButton" data-index="${i}">Excluir</button>
         `
+        }
     }
-}
 
-function editarAluno(index) {
-    if (index >= alunos.length) {
-        throw "indice invalido";
+    const editarAluno = (index)=> {
+        if (index >= alunos.length) {
+            throw "indice invalido";
+        }
+        const nome = document.getElementById("nome").value;
+        const idade = parseInt(document.getElementById("idade").value);
+        const curso = document.getElementById("cursos").value;
+        const notaFinal = parseFloat(document.getElementById("notaFinal").value);
+
+        alunos[index] = new Aluno(nome, idade, curso, notaFinal);
+        atualizarTabela();
     }
-    const nome = document.getElementById("nome").value;
-    const idade = parseInt(document.getElementById("idade").value);
-    const curso = document.getElementById("cursos").value;
-    const notaFinal = parseFloat(document.getElementById("notaFinal").value);
 
-    alunos[index] = new Aluno(nome, idade, curso, notaFinal);
-    atualizarTabela();
-}
-
-function excluirAluno(index){
-    if (index >= alunos.length) {
-        throw "indice invalido";
+    const excluirAluno = (index) =>{
+        if (index >= alunos.length) {
+            throw "indice invalido";
+        }
+        alunos.splice(index, 1);
+        atualizarTabela();
     }
-    alunos.splice(index, 1);
-    atualizarTabela();
-}
+
+    const cadastrarButton = document.getElementById("cadastrarButton");
+    cadastrarButton.addEventListener("click", function (){
+        const nome = document.getElementById("nome").value;
+        const idade = parseInt(document.getElementById("idade").value);
+        const curso = document.getElementById("cursos").value;
+        const notaFinal = parseFloat(document.getElementById("notaFinal").value);
+        buttonCadastrar(new Aluno(nome, idade, curso, notaFinal));
+        alert("Aluno cadastrado com sucesso");
+    });
+
+    const tabelaAlunos = document.getElementById("alunosTableBody");
+    tabelaAlunos.addEventListener("click", (event)=> {
+        const index = parseInt(event.target.dataset.index);
+        if (event.target.classList.contains("editarButton")) {
+            editarAluno(index);
+            alert("Aluno editado com sucesso");
+        }
+        else if (event.target.classList.contains("excluirButton")) {
+            excluirAluno(index);
+            alert("Aluno excluido com sucesso");
+        }
+    });
+})
